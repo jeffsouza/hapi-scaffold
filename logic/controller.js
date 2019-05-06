8ef47e73c64566b629f84f64222594210cd64233
Jefferson Goncalves <jeffsouza@MacBook-Air-de-Jefferson.local>
2019-05-06 18:07:16 -0300

    Added 'edit' and 'delete' features

--- logic/controller.js
+++ logic/controller.js
@@ -7,11 +7,15 @@ const pluralize = require('pluralize');
 class Controller {
 
     static generate(modelName) {
+        const modelNameCapitalized = modelName.charAt(0).toUpperCase() + modelName.slice(1);
+        const modelNameLowered = modelName.toLowerCase();
         const controllerName = pluralize.plural(modelName);
         const controllerNameCapitalized = controllerName.charAt(0).toUpperCase() + controllerName.slice(1);
         const controllerNameLowered = controllerName.toLowerCase();
 
         fs.readFile(path.resolve(__dirname, '../snippets/controller'), function(err, data) {
+            data = data.toString().split('<ModelNameCapitalized>').join(modelNameCapitalized);
+            data = data.toString().split('<ModelNameLowered>').join(modelNameLowered);
             data = data.toString().split('<ControllerNameCapitalized>').join(controllerNameCapitalized);
             data = data.toString().split('<ServiceNameCapitalized>').join(controllerNameCapitalized);
             data = data.toString().split('<ServiceNameLowered>').join(controllerNameLowered);
